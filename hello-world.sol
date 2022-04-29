bbb7fcbb6fe8c66f3d9f6cf58a5d923f4fa1868e
Borja Paz Rodríguez <borjapazr@gmail.com>
2022-04-29 10:46:37 +0200

    chore(init): first solidity sc

---
+++ hello-world.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract SolidityTest {
+    constructor() {}
+
+    function getGreeting() public pure returns (string memory) {
+        return "Hello, friend.";
+    }
+}
